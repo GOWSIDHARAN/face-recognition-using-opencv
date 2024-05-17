22f83c677378b169682607d9ecd83389b2360cbf
GOWSIDHARAN <139457951+GOWSIDHARAN@users.noreply.github.com>
2024-05-17 23:03:50 +0530

    Update 03_face_recognition.py

--- 03_face_recognition.py
+++ 03_face_recognition.py
@@ -2,9 +2,6 @@
 Real Time Face Recogition
 	==> Each face stored on dataset/ dir, should have a unique numeric integer ID as 1, 2, 3, etc                       
 	==> LBPH computed model (trained faces) should be on trainer/ dir
-Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
-
-Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18  
 
 '''
 
