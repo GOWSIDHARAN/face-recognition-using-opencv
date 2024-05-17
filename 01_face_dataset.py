6645135215846ceb480f4580c922fc7d0e4cf3b3
GOWSIDHARAN <139457951+GOWSIDHARAN@users.noreply.github.com>
2024-05-17 23:03:08 +0530

    Update 01_face_dataset.py

--- 01_face_dataset.py
+++ 01_face_dataset.py
@@ -3,9 +3,8 @@ Capture multiple Faces from multiple users to be stored on a DataBase (dataset d
 	==> Faces will be stored on a directory: dataset/ (if does not exist, pls create one)
 	==> Each face will have a unique numeric integer ID as 1, 2, 3, etc                       
 
-Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition    
 
-Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18    
+
 
 '''
 
